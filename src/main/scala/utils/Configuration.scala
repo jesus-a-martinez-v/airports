5132fd4ff82e049fdb8f15bfb2f4a71f59964d6b
Jesús Martínez <jesus.a.martinez.v@gmail.com>
2017-04-17 20:59:59 -0500

    Update Configuration.scala

--- src/main/scala/utils/Configuration.scala
+++ src/main/scala/utils/Configuration.scala
@@ -10,12 +10,12 @@ import scala.concurrent.duration.FiniteDuration
   * App configurations.
   */
 trait Configuration {
-  //Load configuration information from application.conf into memory
+  // Load configuration information from application.conf into memory
   private val config = ConfigFactory.load()
   private val httpConfig = config.getConfig("http")
   private val cacheConfig = config.getConfig("cache")
 
-  //Server parameters.
+  // Server parameters.
   val httpHost: String = httpConfig.getString("interface")
   val httpPort: Int = httpConfig.getInt("port")
   val frequency: FiniteDuration = FiniteDuration(cacheConfig.getDuration("time-to-live", TimeUnit.SECONDS), TimeUnit.SECONDS)
